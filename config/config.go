f59f24fceb63449f671cdd9ee1aed9fb47ff0c40
Fredel <48722410+fredele20@users.noreply.github.com>
2023-02-03 14:59:23 +0000

    first commit: need to fix some errors

---
+++ config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"errors"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+type UniDocConfig struct {
+	Key string
+}
+
+func GetUniDocCred() (*UniDocConfig, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	key := os.Getenv("UNICODE_LICENSE_API_KEY")
+	if len(key) == 0 {
+		return nil, errors.New("unidoc key not found")
+	}
+
+	return &UniDocConfig{Key: key}, nil
+}
