1d807f0255b4c79cf7861ce1915b98ce0f5dda22
fredele20 <victorisholaoladele@gmail.com>
2023-02-04 03:00:23 +0100

    final touch

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"invoice-pdf-generator/internal"
+)
+
 func main() {
 	// Generate sample invoice data
-	// productInvoiceData, err := internal.NewInvoiceData("Product application", 1, 3000.50)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	productInvoiceData, err := internal.NewInvoiceData("Product application", 1, 3000.50)
+	if err != nil {
+		panic(err)
+	}
 
-	// laptopInvoiceData, err := internal.NewInvoiceData("Macbook Pro", 1, 200.70)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	laptopInvoiceData, err := internal.NewInvoiceData("Macbook Pro", 1, 200.70)
+	if err != nil {
+		panic(err)
+	}
 
-	// // Invoice Items collection
-	// invoiceItems := []*internal.InvoiceData{productInvoiceData, laptopInvoiceData}
+	// Invoice Items collection
+	invoiceItems := []*internal.InvoiceData{productInvoiceData, laptopInvoiceData}
 
-	// // Create single invoice
-	// invoice := internal.CreateInvoice("Example Shop1", "Example address", invoiceItems)
-	// err = internal.GenerateInvoicePDF(*invoice)
-	// fmt.Printf("The Total Invoice Amount is: %f", invoice.CalculateInvoiceTotalAmount())
+	// Create single invoice
+	invoice := internal.CreateInvoice("Example Shop1", "Example address", invoiceItems)
+	err = internal.GenerateInvoicePDF(*invoice)
+	fmt.Printf("The Total Invoice Amount is: %f", invoice.CalculateInvoiceTotalAmount())
 }
